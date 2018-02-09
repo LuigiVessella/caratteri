0192d4348d0ab3896eb98a35f3aab7a2d393541c
LuigiVessella <vessellaluigi12@gmail.com>
2018-02-09 19:59:16 +0100

    file

--- contaSpaziature.c
+++ contaSpaziature.c
@@ -4,10 +4,10 @@ int main(){
   int c;
   int spazi=0, tabu=0, newl=0;
   
-  while(c!=EOF){
-    if((c=getchar()) == '\n')newl++;
-    else if((c=getchar()) == ' ')spazi++;
-    else tabu++;
+  while((c = getchar()) != EOF ){
+	  if(c == '\n') newl++;
+	  else if(c == ' ') spazi++;
+	  else tabu++;
   }
 
   printf("Caratteri di tabulazione: %d\nSpazi: %d\nNewline: %d\n", tabu, spazi, newl);
